a7c53f77ce2afcb8f98223fc26b5cf6390d3383f
freddymachado <freddy_machado2@hotmail.com>
2022-06-10 14:48:35 -0400

    add initial pages

---
+++ pages/404.js
@@ -0,0 +1,16 @@
+import Layout from '../components/Layout'
+import Link from 'next/link'
+
+const custom404 = () => (
+    <Layout>
+        <div className='text-center'>
+            
+      <h1>404</h1>
+      <p>Esta página no existe, regresa al  
+           <Link href='/'><a >inicio</a></Link></p>
+        </div>
+    </Layout>
+
+) 
+
+export default custom404;
---
+++ pages/schedule.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout'
+
+const Schedule = () => (
+    <Layout>
+      <h1>Agenda una cita</h1>
+    </Layout>
+
+) 
+
+export default Schedule;
