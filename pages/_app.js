10a438ee86ab914a3f82501da081c3571e3412cf
freddymachado <freddyantoniomachado@gmail.com>
2025-02-19 13:14:09 -0400

    nextjs upgrade

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.css'
-import 'bootswatch/dist/darkly/bootstrap.min.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
