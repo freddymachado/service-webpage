07034a9bdfac0ead732e6308bb86279a7ec6682e
freddymachado <freddyantoniomachado@gmail.com>
2025-02-22 14:00:43 -0400

    preliminar sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,45 @@
 "use client"
 
 import Image from "next/image";
-import { Card, CardContent} from "@/components/ui/card"
+import { Card as Card2, CardContent as CardContent2, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card"
 import Link from "next/link"
-import { Star, Coins, UserCheck, Database } from "lucide-react"
+import { Star, Coins, UserCheck, Database, Check } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import WatchLaterOutlinedIcon from '@mui/icons-material/WatchLaterOutlined';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
 import Flag from 'react-world-flags'
 import { Search} from 'lucide-react'
 import { Input } from "@/components/ui/input"
-
+import { ButtonGroup, Button as Button2 } from "@mui/material";
+import CardMedia from '@mui/material/CardMedia';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardActions from '@mui/material/CardActions';
+const products = [
+  {
+    id: 1,
+    name: 'Contáctanos',
+    category: ['Garantía incluída','Rápido','Confiable',],
+    date: 'Mayo 15, 2023',
+    author: 'María García',
+    image: '/placeholder.svg?height=200&width=300',
+    description:
+      'Puedes hacerlo mediante llamada telefónica, WhatsApp o correo electrónico en cualquier momento',
+  },
+]; 
 export default function Home() {
   //const products = 'await getStripeProducts()';
   //const [searchTerm, setSearchTerm] = useState('');
   const searchTerm = '';
 
-  const handleSearch = () => {
+  /* const handleSearch = () => {
     //setSearchTerm(event.target.value);
-  };
+  }; */
 
   return (
     <>
@@ -62,7 +81,7 @@ export default function Home() {
                     placeholder="Buscar productos..."
                     value={searchTerm}
                     className="pl-8"
-                    onChange={handleSearch}
+                    /* onChange={handleSearch} */
                   />
                 </div>
               </form>
@@ -93,12 +112,12 @@ export default function Home() {
                     Servicio Técnico Especializado para su casa o empresa
                   </h1>
                   <p className=" text-muted-foreground md:text-xl">
-                    Nuestro trabajo incluye garantía
+                    Garantía asegurada en todos nuestros servicios
                   </p>
                 </div>
                 <div className="space-x-4">
                   <Button>Cotizaciones</Button>
-                  <Button>Asesorías</Button>
+                  <Button variant={"outline"}>Asesorías</Button>
                 </div>
               </div>
               <div className="w-full md:w-1/2  flex justify-center">
@@ -115,28 +134,28 @@ export default function Home() {
                     <Coins className="h-6 w-6 text-primary" />
                   </div>
                   <h3 className="text-xl font-bold">Mantenimiento</h3>
-                  <p className="text-muted-foreground text-center">Seamlesly integrate Stripe Billing to capture subscription payments - Webhooks and all</p>
+                  <p className="text-muted-foreground text-center">Descripción corta del servicio de mantenimiento de equipos previamente instalados</p>
                 </div>
                 <div className="flex flex-col items-center space-y-2 border-muted-foreground/10 p-4 rounded-lg">
                   <div className="p-2 bg-primary/10 rounded-full">
                     <UserCheck className="h-6 w-6 text-primary" />
                   </div>
                   <h3 className="text-xl font-bold">Instalación</h3>
-                  <p className="text-muted-foreground text-center"> </p>
+                  <p className="text-muted-foreground text-center"> Descripción breve del servicio de instalación de equipos</p>
                 </div>
                 <div className="flex flex-col items-center space-y-2 border-muted-foreground/10 p-4 rounded-lg">
                   <div className="p-2 bg-primary/10 rounded-full">
                     <Database className="h-6 w-6 text-primary" />
                   </div>
-                  <h3 className="text-xl font-bold">Database</h3>
-                  <p className="text-muted-foreground text-center">Hook into any PostgresDB instance</p>
+                  <h3 className="text-xl font-bold">Otro servicio</h3>
+                  <p className="text-muted-foreground text-center"> Descripción breve del otro servicio </p>
                 </div>
               </div>
             </div>
           </section>
           <section className="w-full py-10 md:py-20 lg:py-32" id="testimonials">
             <div className="container px-4 md:px-6">
-              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-center mb-4">What Our Customers Say</h2>
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-center mb-4">Lo que nuestros clientes opinan</h2>
               <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 mt-8">
                 <Card>
                   <CardContent className="p-6">
@@ -145,8 +164,8 @@ export default function Home() {
                         <Star key={i} className="w-5 h-5 fill-primary text-primary" />
                       ))}
                     </div>
-                    <p className="text-muted-foreground mb-2">&quot;This product has revolutionized our workflow. Highly recommended!&quot;</p>
-                    <p className="font-semibold">- Sarah J., CEO</p>
+                    <p className="text-muted-foreground mb-2">&quot;Buen trabajo y buen precio. Recomendado.&quot;</p>
+                    <p className="font-semibold">- María P.</p>
                   </CardContent>
                 </Card>
                 <Card>
@@ -156,8 +175,8 @@ export default function Home() {
                         <Star key={i} className="w-5 h-5 fill-primary text-primary" />
                       ))}
                     </div>
-                    <p className="text-muted-foreground mb-2">&quot;Wow everything is already integrated! Less time configuring, more time building!.&quot;</p>
-                    <p className="font-semibold">- Mark T., CTO</p>
+                    <p className="text-muted-foreground mb-2">&quot;El Aire acondicionado había pasado por varios técnicos hasta que contraté este servicio, mil gracias.&quot;</p>
+                    <p className="font-semibold">- Eugenia M.</p>
                   </CardContent>
                 </Card>
                 <Card>
@@ -167,67 +186,79 @@ export default function Home() {
                         <Star key={i} className="w-5 h-5 fill-primary text-primary" />
                       ))}
                     </div>
-                    <p className="text-muted-foreground mb-2">&quot;We&aposve seen a 200% increase in productivity since implementing this solution.&quot;</p>
-                    <p className="font-semibold">- Emily R., Operations Manager</p>
+                    <p className="text-muted-foreground mb-2">&quot;El muchacho que vino fue muy educado y dejó todo muy limpio, 100% recomendado.&quot;</p>
+                    <p className="font-semibold">- Elisa C.</p>
                   </CardContent>
                 </Card>
               </div>
             </div>
           </section>
           <section className="w-full py-10 md:py-20 lg:py-32 bg-muted" id="pricing">
-            <div className="container px-4 md:px-6">
-              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-center mb-4">Pricing Plans</h2>
-              <p className="text-muted-foreground text-center mb-8 md:text-xl">Choose the perfect plan for your needs</p>
-              <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-                {/* {products.map((product) => (
-                  <Card key={product.id}>
+            <div className="container px-4 md:px-6 ">
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-center mb-4">¿Buscas un técnico en refrigeración?</h2>
+              <p className="text-muted-foreground text-center mb-8 md:text-xl">Agenda una visita:</p>
+              <div className="text-center w-1/3 m-auto">                
+                 {products.map((product) => (
+                  <Card2 key={product.id}>
                     <CardHeader>
                       <CardTitle>{product.name}</CardTitle>
                       <CardDescription>{product.description}</CardDescription>
                     </CardHeader>
-                    <CardContent>
-                      <p className="text-3xl font-bold">
-                        {product.price.unit_amount
-                          ? `$${(product.price.unit_amount / 100).toFixed(2)}/${product.price.recurring?.interval}`
-                          : 'Custom'}
-                      </p>
-                      <ul className="mt-4 space-y-2">
-                        {product.features?.map((feature, index) => (
-                          <li key={index} className="flex items-center">
+                    <CardContent2>
+
+                      <ButtonGroup variant="outlined" aria-label="Basic button group">
+                        <Button2 variant="outlined"><LocalPhoneOutlinedIcon/></Button2>
+                        <Button2 variant="outlined"><WhatsAppIcon/></Button2>
+                        <Button2 variant="outlined"><EmailOutlinedIcon/></Button2>
+                      </ButtonGroup>
+
+                            {product.category.map((category, index) => (
+                      <ul className="mt-4 space-y-2" key={index}>
+                          
+                              <li key={index} className="flex items-center">
                             <Check className="mr-2 h-4 w-4 text-primary" />
-                            {feature}
+                            <span key={category} className="text-muted-foreground">{category}</span>  
                           </li>
-                        ))}
-                      </ul>
-                    </CardContent>
+                      </ul>))}
+                    </CardContent2>
                     <CardFooter>
                       <Link
                         className="text-sm font-medium hover:underline underline-offset-4 w-full"
-                        href={`/signup?plan=${product.id}`}
+                        href={`/`}
                       >
-                        <Button className="w-full">Get Started</Button>
+                        <Button className="w-full">Agendar</Button>
                       </Link>
                     </CardFooter>
-                  </Card>
-                ))} */}
+                  </Card2>
+                ))} 
               </div>
             </div>
           </section>
           <section className="w-full py-10 md:py-20 lg:py-32 ">
             <div className="container px-4 md:px-6">
-              <div className="flex flex-col items-center justify-center space-y-4 text-center">
-                <div className="space-y-2">
-                  <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Start Your Journey Today</h2>
-                  <p className="max-w-[600px] text-muted-foreground md:text-xl">
-                    Join thousands of satisfied customers and take your business to the next level.
-                  </p>
-                </div>
-                <div className="w-full max-w-sm space-y-2">
-                  <Link className="btn" href="#">
-                    <Button className=" p-7">Get Started</Button>
-                  </Link>
-                </div>
-              </div>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image="/tecnico_refrigeracion.jpg"
+                  alt="tecnico refrigeracion"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    ¿Cómo limpiar tu aire acondicionado?
+                  </Typography>
+                  <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                    Si tu caso es residencial, es posible realizar un mantenimiento preventivo de tu aire acondicionado siguiendo los siguientes pasos...
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button color="primary">
+                  Leer más
+                </Button>
+              </CardActions>
+            </Card>
             </div>
           </section>
         </main>
