e40c3d516cb298efd80b34f274a7af6bf3906bf8
freddymachado <freddyantoniomachado@gmail.com>
2025-02-27 16:48:00 -0400

    Update footer

--- app/page.tsx
+++ app/page.tsx
@@ -263,13 +263,13 @@ export default function Home() {
           </section>
         </main>
         <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-          <p className="text-xs text-muted-foreground">© 2024 Acme Inc. All rights reserved.</p>
+          <p className="text-xs text-muted-foreground">© 2025 Multiservicios Inc. Todos los derechos reservados.</p>
           <nav className="sm:ml-auto flex gap-4 sm:gap-6">
             <Link className="text-xs hover:underline underline-offset-4" href="#">
-              Terms of Service
+              Términos del Servicio
             </Link>
             <Link className="text-xs hover:underline underline-offset-4" href="#">
-              Privacy
+              Privacidad
             </Link>
           </nav>
         </footer>
